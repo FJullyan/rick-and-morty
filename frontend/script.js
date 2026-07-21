84ea31f77ed0697e5711c8c22b43fbed8978cb64
FJullyan <filipejullyanempre@gmail.com>
2026-07-21 17:50:33 -0300

    Adiciona styles

--- frontend/script.js
+++ frontend/script.js
@@ -2,4 +2,22 @@ const API_URL = "http://127.0.0.1:5000";
 async function buscarPersonagens() {
     const resposta = await fetch(API_URL +"/personagens/dashboard");
     const personagens = await resposta.json();
+    const cards = document.getElementById("cards");
+    personagens.forEach(personagem => {
+    cards.innerHTML += `
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="${personagem.image}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${personagem.name}</h5>
+                    <p>Status: ${personagem.status}</p>
+                    <p>Espécie: ${personagem.species}</p>
+                    <p>Origem: ${personagem.origin}</p>
+                    <p>Episódios: ${personagem.total_episodios}</p>
+                </div>
+            </div>
+        </div>
+    `;
+});
 }
+buscarPersonagens();
